97bae830b5fbc3474dc2a85df70809cf2fa8cc67
seena18 <seena101.sa@gmail.com>
2022-04-01 13:31:18 -0700

    delete functionality

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import Table from './Table'
-const characters = [
+import React, { useState } from 'react';
+import Table from './Table';
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
     {
       name: 'Charlie',
       job: 'Janitor',
@@ -17,13 +19,19 @@ const characters = [
       name: 'Dennis',
       job: 'Bartender',
     },
-];
-function MyApp() { 
-    return ( 
-      <div className = "container"> 
-        <Table characterData={characters}/>
-      </div> 
-    );  
-}   
+  ]);
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    </div>
+  );
+}
+
 
 export default MyApp;
