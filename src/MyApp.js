1b97aeb5c9fafe43d68676fa78c8be010ba1d5ee
seena18 <seena101.sa@gmail.com>
2022-04-10 12:16:42 -0700

    linked with backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,23 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from './Table';
 import Form from './Form';
-
+import axios from 'axios'
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
-
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index
+  useEffect(() => {
+    fetchAll().then(result => {
+      if (result)
+        setCharacters(result);
     });
-    setCharacters(updated);
+  }, []);
+  function removeOneCharacter(index) {
+    let person = characters[index];
+    console.log(person);
+    makeDeleteCall(person.id).then(result => {
+      if (result && result.status === 204) {
+        const updated = characters.filter((character, i) => {
+          return i !== index
+        });
+        setCharacters(updated);
+      }
+    });
   }
-
-
+  async function fetchAll() {
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post('http://localhost:5000/users/', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async function makeDeleteCall(person) {
+    try {
+      const response = await axios.delete(`http://localhost:5000/users/${person}`, person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
   function updateList(person) {
-    setCharacters([...characters, person]);
+    makePostCall(person).then(result => {
+      if (result && result.status === 201) {
+        console.log(result.data.id);
+        console.log(Array.isArray(characters));
+        const updated = characters.concat(result.data);
+        setCharacters(updated);
+
+      }
+    });
   }
+
 
 
   return (
--- src/Table.js
+++ src/Table.js
@@ -7,19 +7,22 @@ function TableHeader() {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
+                <th>id</th>
             </tr>
         </thead>
     );
 }
 
 function TableBody(props) {
+    console.log(typeof props.characterData);
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>{row.id}</td>
                 <td>
-                    <button onClick={()=>props.removeCharacter(index)}>Delete</button>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
                 </td>
             </tr>
         );
